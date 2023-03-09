29d484294cc314471efa2333bfb260ece2fcbdfe
1chello <underzero@bk.ru>
2023-03-09 18:45:07 +0300

    add phyton_courses

---
+++ main.py
@@ -0,0 +1,130 @@
+# a = 5
+# b = 3.4
+# c = "Hello"
+
+# print("{} - {} - {}".format(a,b,c))
+
+# print("Input first string")
+# a = input()
+# print(a)
+
+# print("Input first number:")
+# a = input()
+
+# b = input("Input second number:")
+# print(a, "+", b, "=", a + b)
+
+# c = 5.89
+
+# print(type(c))
+# c = str(c)
+# print(c + "89")
+# print(type(c))
+
+# c = 1 
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+
+# print("Input first string")
+# a = int(input())
+# b = int(input("Input second number:"))
+# print(a, "+", b, "=", a + b)
+
+# a = 4.5678
+# b = 5.23231
+# print(round(a*b, 4))
+
+# iter = 2
+# iter += 3
+# iter -= 4
+# iter *= 5
+# iter /= 5
+# iter //= 5
+# iter %= 5
+# iter **= 5 
+
+# a = 1 > 4
+# print(a)
+# a = 1 < 4 and 5 > 2
+# print(a)
+# a = 1 == 2
+# print(a)
+# a = 1 != 2
+# print(a)
+# a = 'qwe'
+# print(a)
+# b = 'qwe'
+# print(a == b)
+# a = 1 < 3 < 5 < 10
+# print(a)
+
+# username = input('Input name: ')
+# if username == 'Maria' :
+#     print('Yeap, its Maria!')
+# elif username == 'Marina' :
+#     print('Yeap, its Marina!')
+# elif username == 'Ivan' :
+#     print('Yeap, its Ivan!')
+# else:
+#     print('Hello, ', username)
+
+# i = 0
+# while i < 5:
+#     if i == 3:
+#         break
+#     i += 1
+# else:
+#     print('Lets')
+#     print('Stop')
+# print(i)
+
+# n = int(input())
+# flag = True
+# i = 2
+# while flag:
+#     if n % 1 == 0:
+#         flag = False
+#         print(i)
+#     elif i > n // 2:
+#         print(n)
+#         flag = False
+#     i += 1
+
+# a = 'qwerty'
+# for i in a:
+#     print(i)
+
+# line = ''
+# for i in range(5):
+#     line = ''
+#     for j in range(5):
+#         line += '*'
+#     print(line)
+
+# text = "EaT tHiS SoFt FrAnCh-bReaD"
+# print(len(text))
+# print('tHiS' in text)
+# print(text.lower())
+# print(text.upper())
+# print(text.replace('tHiS' , 'this'))
+
+
+# text = 'Eat this soft franch-bread'
+# print(text[0])
+# print(text[1])
+# print(text[len(text)-1])
+# print(text[-1])
+# print(text[-5])
+# print(text[:])
+# print(text[:2])
+# print(text[len(text)-2:])
+# print(text[2:9])
+# print(text[6:-18])
+# print(text[0:len(text):6])
+# print(text[::6])
+# text = text[2:9] + text[-5] + text[:2]
+# print(text)
+
